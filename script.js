db18929319417ef1ff082e2366836b8007efbc36
AdriZonari <36879308+AdriZonari@users.noreply.github.com>
2018-03-19 00:13:54 +0100

    hotel clock

--- script.js
+++ script.js
@@ -1,81 +1,55 @@
-(function() {
-"use strict";
+(function(){
+'use strict';
 
-let state = document.getElementById('s-state');
+document.addEventListener('DOMContentLoaded', function(){
+	let cTime = document.getElementById('current-time'),
+		cDate = document.getElementById('current-date');
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);/* Listen to what comes from submit form. Fire the function to estimate total*/
+		
+	
+		setInterval(function() {
+		let d = new Date();
+	
+		let hours = d.getHours(),
+			minutes = d.getMinutes(),
+			day = d.getDate(),
+			month = formatMonth(d.getMonth()),
+			ampm = 'AM';
+		
+		if (hours > 12) {
+			hours -= 12;
+			ampm = 'PM';
+		}else if (hours === 0) {
+			hours = 12;
+		}
+		 if (minutes < 10) {
+			minutes = '0' + minutes;
+		}
+		let sepClass = '';
+		if (d.getSeconds() % 2 === 1) sepClass = 'trans';
 
-    let btnEstimate = document.getElementById('btn-estimate');
+		let sep = '<span class="' + sepClass + '">:</span>';
+	
+		cTime.innerHTML =  hours + sep + minutes + ' ' + ampm;
+		cDate.textContent = month + ' ' + day;
+	}, 1000);
+	
+	function formatMonth(m) {
+		m = parseInt(m, 10);
 
-    btnEstimate.disabled = true;
+		if (m < 0) {
+			m = 0;
+		}else if (m > 11) {
+			m = 11;
+		}
 
-    state.addEventListener('change', function() {
-        
-        if(state.value === '') {
-            btnEstimate.disabled = true;
-           } else {
-            btnEstimate.disabled = false;
-           }
-        
+		let monthNames = [
+			'January', 'February', 'March', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December'
 
-    });
+		]
+		return monthNames[m];
+	}
 });
 
-
-
-
-function estimateTotal(e) {
-    e.preventDefault(); /* Default is that the pages upload after the submisson of the form*/
-       
-    if (state.value === '') {
-        alert("Please choose a shipping destination");
-    
-        state.focus();
-    }
-
-    let itemBball = parseInt(document.getElementById('txt-q-bball').value, 10) || 0,
-        itemJersey = parseInt(document.getElementById('txt-q-jersey').value, 10) || 0,
-        itemPower = parseInt(document.getElementById('txt-q-power').value, 10) || 0 ,
-        itemWater = parseInt(document.getElementById('txt-q-water').value, 10) || 0,
-        shipingState = state.value,
-        shipingMethod = document.querySelector('[name=r_method]:checked').value || " ";
-    
-    let totalQty = itemBball + itemJersey + itemPower + itemWater,
-        shippingCostPer,
-        totalShippingCost,
-        taxFactor = 1,
-        estimate,
-        totalPrice = (90 * itemBball) + (25 * itemJersey) + (30 * itemPower) + (4 * itemWater);
-
-        if (shipingState === 'CA'){
-            taxFactor = 1.075;
-        } else if(shipingState === 'WA'){
-            taxFactor = 1.065;
-        }
-
-        if (shipingMethod == 'pickup'){
-            shippingCostPer = 0;
-        }else if (shipingMethod == 'usps'){
-            shippingCostPer = 2;
-        }else if (shipingMethod == 'ups'){
-            shippingCostPer = 3;
-        }
-
-        totalShippingCost = shippingCostPer * totalQty;
-
-        estimate = '$' + ((totalPrice * taxFactor) + totalShippingCost).toFixed(2);
-
-        document.getElementById('txt-estimate').value = estimate;
-
-        let results = document.getElementById('results');
-
-        results.innerHTML = 'Total items: ' + totalQty + '<br>';
-        results.innerHTML += 'Total shipping cost: $' + totalShippingCost.toFixed(2) + '<br>'; 
-        results.innerHTML += 'Tax: ' + ((taxFactor-1)*100).toFixed(2) + '% (' + shipingState + ')';
-        //total items
-        //total shipping cost
-        //tax
-    }
 
 })();
