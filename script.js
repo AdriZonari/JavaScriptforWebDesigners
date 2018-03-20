40fab4973fc3eff8a546682510c9dff0f4951980
AdriZonari <36879308+AdriZonari@users.noreply.github.com>
2018-03-21 00:07:58 +0100

    Google Map API

--- script.js
+++ script.js
@@ -1,55 +1,41 @@
-(function(){
-'use strict';
+var map;
+      function initMap() {
+        var hotellLocation = {lat: 55.4437498, lng: 13.0310812},
+            cityStore = {lat:55.6111121, lng: 12.9779265},
+            centerPoint = {lat:55.55910948701171, lng: 13.029802980983959}; //in console map.getCenter().toString(); to get this position
+        
+        map = new google.maps.Map(document.getElementById('hplus-map'), {
+            'zoom': 10,
+            'center': centerPoint, 
+            'mapTypeId': google.maps.MapTypeId.ROADMAP,
+            'draggable': true,
+            'scrollwheel': false
+        });
+        var contentString = '<div><h1>Ängavallen, Gårdshotell, Restaurang, Gårdsbutik, Café</h1></div>;'
+        var contentCityStore = '<div><h1>Ängavallen, City Farm Shop</div>;'
+        var infowindowHotell = new google.maps.InfoWindow({
+        'content': contentString, 
+        });
+        var infowindowCity = new google.maps.InfoWindow({
+            'content': contentCityStore, 
+            });
+        var marker = new google.maps.Marker({
+        'position': hotellLocation, 
+        'map': map,
+        'title': 'Ängavallen',
+        });
 
-document.addEventListener('DOMContentLoaded', function(){
-	let cTime = document.getElementById('current-time'),
-		cDate = document.getElementById('current-date');
-
-		
-	
-		setInterval(function() {
-		let d = new Date();
-	
-		let hours = d.getHours(),
-			minutes = d.getMinutes(),
-			day = d.getDate(),
-			month = formatMonth(d.getMonth()),
-			ampm = 'AM';
-		
-		if (hours > 12) {
-			hours -= 12;
-			ampm = 'PM';
-		}else if (hours === 0) {
-			hours = 12;
-		}
-		 if (minutes < 10) {
-			minutes = '0' + minutes;
-		}
-		let sepClass = '';
-		if (d.getSeconds() % 2 === 1) sepClass = 'trans';
-
-		let sep = '<span class="' + sepClass + '">:</span>';
-	
-		cTime.innerHTML =  hours + sep + minutes + ' ' + ampm;
-		cDate.textContent = month + ' ' + day;
-	}, 1000);
-	
-	function formatMonth(m) {
-		m = parseInt(m, 10);
-
-		if (m < 0) {
-			m = 0;
-		}else if (m > 11) {
-			m = 11;
-		}
-
-		let monthNames = [
-			'January', 'February', 'March', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December'
-
-		]
-		return monthNames[m];
-	}
+        var markerCity = new google.maps.Marker({
+            'position': cityStore, 
+            'map': map,
+            'title': 'Ängavallen, city',
+            });
+        
+        marker.addListener('click', function() {
+            infowindowHotell.open(map, (marker));
+        
+        markerCity.addListener('click', function() {
+                infowindowCity.open(map, (markerCity));
+          });
 });
-
-
-})();
+      }
